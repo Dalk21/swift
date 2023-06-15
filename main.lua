29b79f802818b72474fd046dc26ca93d98475911
Dalk <102332227+Dalk21@users.noreply.github.com>
2023-06-15 03:13:00 -0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,8 @@
+print('SWIFT HUB is DISCONTINUED as of June 15th 2023')
+
+local ui = Instance.new('ScreenGui')
+local tb = Instance.new('TextLabel')
+ui.Parent = game.CoreGui
+tb.Parent = ui
+tb.Size = UDim2.new(1, 0, 1, 0)
+tb.Text = "SWIFT HUB is DISCONTINUED AS OF JUNE 15th 2023"
